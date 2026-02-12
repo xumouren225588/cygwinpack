96226e16206657ca3a134b1e52eaee021cac6d39
xumouren225588 <xumouren225588@163.com>
2026-02-12 12:06:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 import datetime
 
 setup(
-    name="cygwin_pack",
+    name="cygwinpack",
     version=datetime.datetime.now().strftime("%Y%m%d"),
-    packages=["cygwin_pack"],
+    packages=["cygwinpack"],
     package_data={
         "package": ["cygwin/*"],  # 包含 tools 目录下的所有 .exe
     },
     entry_points={
         "console_scripts": [
-            "cygwin_pack=cygwin_pack.cli:main",  # ← 安装后可用 runmyexe 命令
+            "cygwinpack=cygwinpack.cli:main",  # ← 安装后可用 runmyexe 命令
         ],
     },
     include_package_data=True,
     python_requires=">=3.10",
     platforms=["Windows"]
+
 )
